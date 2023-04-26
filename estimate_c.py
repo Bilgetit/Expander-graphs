e2fd14b25507a96e4e7d4da3307632d328564d3f
Mikkel Gjestrud <m.gjestrud@gmail.com>
2023-04-26 14:27:39 +0200

    made estimate_c work as a class, and use random_matrix to make it faster. Cleaned up a bit as well

--- estimate_c.py
+++ estimate_c.py
@@ -1,50 +1,93 @@
 import numpy as np
-from tuple_graph import get_graph, get_edges, check_start_matrix
+from tuple_graph import get_graph, get_edges
 from typing import Optional
 from tuple2matrix import tuple2matrix
 from boundary import find_boundary
+from random_matrix import get_matrix
 from size import degree
-import random
 import time
 
 
-def estimate_c(
-    n: int,
-    p: int,
-    times: int = 1000,
-    end: Optional[int] = None,
-    start_matrix: Optional[np.ndarray] = None,
-) -> float:
-    """Estimate c."""
-    whole_graph = get_graph(n, p)
-    max = degree(n, p)
-    c = []
-    for _ in range(times):
-        if end:
-            stop = random.randint(1, end-1)
+class estimate:
+    """class for estimating c, generating random subsets, and calculating the edges."""
+
+    def __init__(
+        self,
+        n: int = 3,
+        p: int = 5,
+        times: int = 1000,
+        subset_end: Optional[int] = None,
+        matrix_end: Optional[int] = None,
+        start_matrix: Optional[np.ndarray] = None,
+    ):
+        self.n = n
+        self.p = p
+        self.times = times
+        self.subset_end = subset_end
+        self.matrix_end = matrix_end
+        self.start_matrix = start_matrix
+        self.degree = degree(self.n, self.p)
+        self.c = None
+
+    def worker(self):
+        """worker for multiprocessing. Estimates single c."""
+        if self.subset_end is not None:
+            self.subset_stop = np.random.randint(1, self.subset_end - 1)
         else:
-            stop = random.randint(1, max-1)
-        start_matrix = random.choice(tuple(whole_graph))
-        start_matrix = tuple2matrix(start_matrix, n)
-        boundary, len_boundary = find_boundary(n, p, size = stop, starting_matrix=start_matrix)
-        c.append(len_boundary / ((1 - stop / max)*stop))           # c = |B| / (1 - |A|/|V|) * |A|. Find better way of len(boundary)?
-    return c
+            self.subset_stop = np.random.randint(1, self.degree - 1)
+            """stop will be the size of the subset we are taking the boundary of."""
+
+        if self.start_matrix is not None:
+            # self.stop = np.random.randint(1, 500)
+            self.start_matrix = get_matrix(
+                self.n, self.p, stop=self.stop, starting_matrix=self.start_matrix
+            )
+            """get_matrix now starts from a random matrix in the graph, given by the last self.start_matrix."""
+        else:
+            # self.stop = np.random.randint(1, 500)
+            self.stop = 300
+            self.start_matrix = get_matrix(self.n, self.p, stop=self.stop)
+
+        boundary = find_boundary(
+            self.n, self.p, size=self.subset_stop, starting_matrix=self.start_matrix
+        )
+        c = len(boundary) / ((1 - self.subset_stop / self.degree) * self.subset_stop)
+
+        if c < self.c:
+            self.c = c
+
+    def do_work(self):
+        for _ in range(self.times):
+            self.worker()
+
+    def main(self):
+        self.c = 10000
+        self.do_work()
+        return self.c
+
 
 def time_estimate_c(
     n: int,
     p: int,
-    times: int = 1000,
-    end: Optional[int] = None,
+    times: int = 100,
+    subset_end: Optional[int] = None,
     start_matrix: Optional[np.ndarray] = None,
-) -> float:
+) -> list[float]:
     """Timer for estimate_c."""
     t_0 = time.time()
-    c = estimate_c(n, p , times = times)
+    instance = estimate(n, p, times, subset_end, start_matrix)
+    c = instance.main()
     t_1 = time.time()
-    print(f"{t_1-t_0=}")
-    # print(c)
-    print(f"min = {min(c)}")
+    print(f"n = {n}, p = {p}, times = {times}")
+    print(f"t_1 - t_0 = {t_1-t_0}")
+    print(f"c = {c}")
     return c
 
+def get_c(n: int, p: int, times: int = 100, subset_end: Optional[int] = None, start_matrix: Optional[np.ndarray] = None) -> float:
+    instance = estimate(n, p, times, subset_end, start_matrix)
+    c = instance.main()
+    return c
+
+
 if __name__ == "__main__":
-    time_estimate_c(2, 13, times = 100)
+    time_estimate_c(2, 17, times=250)
