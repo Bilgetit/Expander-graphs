51c998398b5e2cee15d230bbe4e0d85470d554f8
Mikkel Gjestrud <m.gjestrud@gmail.com>
2023-05-07 14:52:05 +0200

    fixed 3d plot, got it working faster

--- plot_c.py
+++ plot_c.py
@@ -8,6 +8,7 @@ from sympy import isprime
 import time
 from size import degree
 from estimate_c import get_c
+import pickle
 
 
 def plot_c_against_A(n: int = 3, p: int = 5, plot = False) -> None:
@@ -27,8 +28,8 @@ def plot_c_against_A(n: int = 3, p: int = 5, plot = False) -> None:
     return c
 
 
-def plot_c_against_p(n: int, perc: float = 0.1, plot: bool = False) -> None:
-    primes = [i for i in range(11, 110) if isprime(i)]
+def plot_c_against_p(n: int, max_p:int = 50, perc: float = 0.15, plot: bool = False) -> None:
+    primes = [i for i in range(11, max_p) if isprime(i)]
     with Pool() as pool:
         c = pool.starmap(calc_c_p, [(n, p, perc) for p in primes])
     if plot:
@@ -40,31 +41,46 @@ def plot_c_against_p(n: int, perc: float = 0.1, plot: bool = False) -> None:
         plt.show()
     return c
 
-def plot_c_3d(n: int = 2, max_p: int = 101, min_perc: float = 0.01, max_perc: float = 0.2, plot: bool = False) -> None:
+def plot_c_3d(n: int = 2, max_p: int = 101, min_perc: float = 0.01, max_perc: float = 0.2, plot: bool = True, filename: str = 'FigureObject.fig.pickle') -> None:
     primes = [i for i in range(11, max_p) if isprime(i)]
     X = np.array(primes)
-    perc = np.linspace(min_perc, max_perc, int(len(primes)))
+    length = int(len(primes))
+    perc = np.linspace(min_perc, max_perc, length)
     Y = np.array(perc)
     X, Y = np.meshgrid(X, Y)
-    c = []
-    for p in primes:
-        c.append(np.array(c_worker(n=n, p=p, perc=perc)))
+    # c = []
+    # for percent in perc:
+    #     c.append(np.array(c_worker(n=n, primes=primes, perc=percent)))
+    with Pool() as pool:
+        c = pool.starmap(c_worker, [(n, primes, i) for i in perc])
     c = np.array(c)
     print(c)
+    min_c = np.min(c)
+    print(f"min: {min_c}")
+    index = np.where(c == min_c)
+    print(f"min i: {index}")
+    p_i, perc_i = index[1][0], index[0][0]
+    print(f"min p: {primes[p_i]}")
+    print(f"min perc: {perc[perc_i]}")
 
     if plot:
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
         surf = ax.plot_surface(X, Y, c, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
+        # ax.scatter(X, Y, c, cmap=cm.coolwarm)
         ax.yaxis.set_major_formatter(mtick.PercentFormatter())
         plt.xlabel("p")
         plt.ylabel("|A|")
+        pickle.dump(fig, open(filename, 'wb'))
         # plt.clabel("c")
-        plt.show()
+        # plt.show()
 
-def c_worker(n: int, p: int, perc: np.array) -> float:
-    with Pool() as pool:
-        c = pool.starmap(calc_c_p, [(n, p, percent) for percent in perc])
+def c_worker(n: int, primes: np.array, perc: int) -> float:
+    # with Pool() as pool:
+    #     c = pool.starmap(calc_c_p, [(n, p, perc) for p in primes])
+    c = []
+    for p in primes:
+        c.append(calc_c_p(n=n, p=p, perc=perc))
     return c
 
 
@@ -73,11 +89,19 @@ def calc_c_p(n: int, p: int, perc: float = 0.1) -> float:
     c = get_c(n=n, p=p, subset_end=size)
     return c
 
+def show_3d_figure():
+    fig = pickle.load(open('FigureObject.fig.pickle', 'rb'))
+    plt.show()
+
 
 if __name__ == "__main__":
     t_0 = time.time()
     # plot_c_against_A(n=2, p=13, plot=True)
-    # plot_c_against_p(n=2, perc=0.1, plot=True)
-    plot_c_3d(n=2, max_p=110, min_perc=0.05, max_perc=0.2, plot=True)
+    # plot_c_against_p(n=2, max_p = 150, perc=0.15, plot=True)
+    plot_c_3d(n=2, max_p=30, min_perc=0.01, max_perc=0.4, plot=True)
+    # show_3d_figure()
     t_1 = time.time()
     print(f"Time taken: {t_1 - t_0}")
+    # plt.show()
+
+
